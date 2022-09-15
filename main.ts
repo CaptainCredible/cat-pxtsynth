64883477f29471f4a9103ea742cdbe4eb78f6262
CaptainCredible <cpt.credible@gmail.com>
2022-09-15 22:53:28 +0200

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -5,8 +5,6 @@ let myOnTime = 0
 let myOnTimer = 0
 let altTrack = 1 //1 = Zim, 0 = Zam
 let poly = false
-let perxles: neopixel.Strip = null
-perxles = neopixel.create(DigitalPin.P1, 24, NeoPixelMode.RGB)
 let midiChannel: midi.MidiController = null
 let anOutputIsOn = false
 let chan = 0
@@ -35,7 +33,7 @@ function sendMonoMidiViaI2C(port: number) {
 function sendMonoMidi() {
     if (myNote != 127) {
         midiChannel.noteOn(myNote)
-        led.toggle(2, 2)
+        //led.toggle(2, 2)
         midiChannel.noteOff(myNote)
     }
 }
@@ -43,7 +41,7 @@ function sendMonoMidi() {
 let altOutPut = 0;
 radio.onReceivedValue(function (name, value) {
     if (name == "CatP") {
-        led.toggleAll()
+        //led.toggleAll()
         bitCheckMask = 1
         for (let i = 0; i <= 16 - 1; i++) {
             if (bitCheckMask & value) {
@@ -105,8 +103,7 @@ radio.onReceivedValue(function (name, value) {
 
         if (value & 0b01000000) {
             mumMuted = true
-            led.plot(4, 4)
-            //   basic.showIcon(IconNames.No, 0)
+               basic.showIcon(IconNames.No, 0)
         } else if (mumMuted) {
             mumMuted = false
             //   basic.clearScreen()
@@ -116,12 +113,7 @@ radio.onReceivedValue(function (name, value) {
 
 })
 
-function handleNeoPixies() {
-    for (let indeks = 0; indeks <= 2; indeks++) {
-        perxles.setPixelColor(myNote + indeks, neopixel.colors(NeoPixelColors.Red))
-        perxles.show()
-    }
-}
+
 
 function sendMidi() {
     MIDIoffset = 48
@@ -129,20 +121,19 @@ function sendMidi() {
     // led.toggle(0, 0)
     midiChannel.noteOff(myNote + MIDIoffset)
 }
-input.onButtonPressed(Button.A, () => {
-    noteOffset -= 12
-    midiChannel = midi.channel(chan)
+
+input.onButtonPressed(Button.B, function() {
+    synthBlocks.importPresetString(SynthPreset.Sound1, "{ OscType::Triangle, OscType::Triangle, 12.000000, 1.000000, 1.000000, 0.000000, 0.000000, 0.300000, 0.300000, 0.008000, FilterType::LPF, 0.208000, 0.000000, 0.596000, 0.000000, 1.000000, 0.000000, 0.100000, 0.192000, 1.020000, OscType::Triangle, 3.220000, 2.920000, 0.040000, 0.624000, 0.000000, 0.000000, false }")
+    synthBlocks.playSynthNote(Note.C, 200, 127, SynthPreset.Sound1)
 })
 
-input.onButtonPressed(Button.B, () => {
-    noteOffset += 12
-    //midiChannel = midi.channel(chan)
+input.onButtonPressed(Button.A, function () {
+    synthBlocks.importPresetString(SynthPreset.Sound1, "{ OscType::Pulse, OscType::Pulse, 0.192000, 0.500000, 0.552000, 0.000000, 0.000000, 0.300000, 0.300000, 0.000000, FilterType::LPF, 0.400000, 0.000000, 0.300000, 0.100000, 0.000000, 0.000000, 0.500000, 0.500000, 1.000000, OscType::Triangle, 3.220000, 13.560000, 0.184000, 0.500000, 0.000000, 0.000000, false }")
+    synthBlocks.playSynthNote(Note.C, 200, 127, SynthPreset.Sound1)
 })
 
-function turnOffAllLeds() {
-    perxles.clear()
-    perxles.show()
-}
+
+
 function handleOutput() {
     if (!mumMuted) {
         led.plot(0, outPut % 5)
@@ -157,7 +148,7 @@ function handleOutput() {
 }
 
 synthBlocks.importPresetString(SynthPreset.Sound1, "{ OscType::Pulse, OscType::Pulse, 0.192000, 0.500000, 0.552000, 0.000000, 0.000000, 0.300000, 0.300000, 0.000000, FilterType::LPF, 0.400000, 0.000000, 0.300000, 0.100000, 0.000000, 0.000000, 0.500000, 0.500000, 1.000000, OscType::Triangle, 3.220000, 13.560000, 0.184000, 0.500000, 0.000000, 0.000000, false }")
-synthBlocks.playSynthNote(Note.C, 200, 127, SynthPreset.Sound1)
+//synthBlocks.playSynthNote(Note.C, 200, 127, SynthPreset.Sound1)
 
 function handleAltOutput() {
     if (!catMuted) {
@@ -166,13 +157,13 @@ function handleAltOutput() {
         led.plot(altOutPut % 5, 2)
         led.plot(altOutPut % 5, 3)
         led.plot(altOutPut % 5, 4)
+        
         myOnTimer = input.runningTime() + myOnTime
         myNote = altOutPut;
         orchestra.setParameter(SynthPreset.Sound1, SynthParameter.EnvRelease, 0.3);
         orchestra.note(myNote, 100, 127, SynthPreset.Sound1)
-        handleNeoPixies();
-
-
+        bryte = 255
+        updateLedPulse(bryte)
         //sendMonoMidiViaI2C(1)
     }
 }
@@ -184,9 +175,9 @@ let outPut = 0
 let myPins: number[] = []
 basic.showLeds(`
     . . . . .
-    . # . . .
-    . # # . .
     . # # # .
+    . # # # .
+    . . . . .
     . . . . .
     `)
 basic.pause(500)
@@ -195,15 +186,27 @@ myPins = [9, 15, 20, 21, 22, 23]
 list = 0
 radio.setGroup(83)
 music.setTempo(200)
-basic.forever(() => {
+let bryte = 255;
+basic.forever(function () {
     if (input.runningTime() > myOnTimer) {
         if (!muted) {
-            basic.clearScreen()
+            led.plot(1, 1)
+            led.plot(2, 1)
+            led.plot(3, 1)
+
+            led.plot(1, 2)
+            led.plot(2, 2)
+            led.plot(3, 2)
+
         } else {
             basic.showIcon(IconNames.No, 1)
         }
+        updateLedPulse(bryte)
+        
 
-        led.plot(0, 0)
+        bryte -=15
+
+
         // turnOffAllLeds()
         //for (let offIndex = 0; offIndex <= 6 - 1; offIndex++) {
         //    pins.digitalWritePin(myPins[offIndex], 0)
@@ -217,4 +220,39 @@ basic.forever(() => {
     let tempParam = pins.map(input.acceleration(Dimension.X), -2048, 2048, 0, 1);
     orchestra.setParameter(SynthPreset.Sound1, SynthParameter.Cutoff, tempParam)
     orchestra.setParameter(SynthPreset.Sound1, SynthParameter.FilterEnvAmount, tempParam)
+    let myZ = Math.map(input.acceleration(Dimension.Z), -1024, 1024, -1, 1);
+    let myFreq = Math.map(myZ, -1, 1, 0, 8);
+    orchestra.setParameter(SynthPreset.Sound1, SynthParameter.VibratoAmount, myZ)
+    orchestra.setParameter(SynthPreset.Sound1, SynthParameter.VibratoFreq, myFreq)
 })
+
+function updateLedPulse(brght: number){
+    let line = 0
+    led.plotBrightness(0, line, brght)
+    led.plotBrightness(1, line, brght)
+    led.plotBrightness(2, line, brght)
+    led.plotBrightness(3, line, brght)
+    led.plotBrightness(4, line, brght)
+
+    line = 1
+    led.plotBrightness(0, line, brght)
+    led.plotBrightness(4, line, brght)
+
+    line = 2
+    led.plotBrightness(0, line, brght)
+    led.plotBrightness(4, line, brght)
+
+    line = 3
+    led.plotBrightness(0, line, brght)
+    led.plotBrightness(1, line, brght)
+    led.plotBrightness(2, line, brght)
+    led.plotBrightness(3, line, brght)
+    led.plotBrightness(4, line, brght)
+
+    line = 4
+    led.plotBrightness(0, line, brght)
+    led.plotBrightness(1, line, brght)
+    led.plotBrightness(2, line, brght)
+    led.plotBrightness(3, line, brght)
+    led.plotBrightness(4, line, brght)
+}
